ede0dfbe1e91e177fbf73be9ada21c7ceb6e1d4e
Alexander <alex030t@stud.kea.dk>
2021-03-09 21:30:14 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/controllers/ThereBeDragonsController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ThereBeDragonsController {
+
+
+    @GetMapping("/")
+    public String mainPage(){
+        return "index.html";
+    }
+
+    @GetMapping("/about")
+    public String aboutPage(){
+
+        return "about.html";
+    }
+
+    @GetMapping("/work")
+    public String workPage(){
+
+        return "work.html";
+    }
+
+    @GetMapping("/gallery")
+    public String gallerayPage(){
+        return "gallery.html";
+    }
+}
